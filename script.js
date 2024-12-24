704781d8d6d8d6a25c0aca8631b1efc0c8d1761e
Shannon406 <shannondidlick95@gmail.com>
2024-12-24 19:44:28 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Open the Google Maps link
+function openMap() {
+  const mapUrl = "https://www.google.com/maps/embed?pb=!4v1735059909074!6m8!1m7!1siWCYgOlG_oi4kLfdrz6W4g!2m2!1d50.68004356812782!2d-3.463687452139472!3f257.8847260335149!4f-11.196874290874888!5f0.7820865974627469";
+  window.open(mapUrl, "_blank");
+}
